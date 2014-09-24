f72ac2fe313bd60da4594c82747fa01a21e0d84e
Mohamed <mo7amed.elshaf3y@gmail.com>
2014-09-24 21:37:23 +0300

    adding pdf generation

--- application/views/preview.php
+++ application/views/preview.php
@@ -6,12 +6,12 @@
         <div style="float: left;width: 100%">
             <span class="card-info" style="top:435px;font-weight: bold;color:rgb(39,155,212)"><?php echo $_REQUEST['name_en'] ?></span>
             <span class="card-info" style="top:440px"><?php echo $_REQUEST['title_en'] ?></span>
-            <span class="card-info" style="top:710px"><?php echo $_REQUEST['tel'] ?></span>
-            <span class="card-info" style="top:720px"><?php echo $_REQUEST['direct'] ?></span>
-            <span class="card-info" style="top:730px"><?php echo $_REQUEST['fax'] ?></span>
-            <span class="card-info" style="top:735px"><?php echo $_REQUEST['email'] ?></span>
-            <span class="card-info" style="top:750px"><?php echo $_REQUEST['web'] ?></span>
-            <span class="card-info" style="top:765px"><?php echo $_REQUEST['charity'] ?></span>
+            <span class="card-info" style="top:653px"><?php echo $_REQUEST['tel'] ?></span>
+            <span class="card-info" style="top:664px"><?php echo $_REQUEST['direct'] ?></span>
+            <span class="card-info" style="top:674px"><?php echo $_REQUEST['fax'] ?></span>
+            <span class="card-info" style="top:678px"><?php echo $_REQUEST['email'] ?></span>
+            <span class="card-info" style="top:689px"><?php echo $_REQUEST['web'] ?></span>
+            <span class="card-info" style="top:699px"><?php echo $_REQUEST['charity'] ?></span>
             <img  src="<?php echo site_url('business-cards/En-Front.jpg') ?>">
         </div>
         <style>
